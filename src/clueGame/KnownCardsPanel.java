8db209ee416ccb6896602a784a6b07ca75fddb8e
Joshua Schlatter <jschlatter@mines.edu>
2022-04-07 13:45:46 -0600

    Created a grid size that is applicable to each Panel. Also added the correct text fields to each panel.

--- src/clueGame/KnownCardsPanel.java
+++ src/clueGame/KnownCardsPanel.java
@@ -2,6 +2,7 @@ package clueGame;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 import java.util.Set;
 
 import javax.swing.*;
@@ -9,8 +10,9 @@ import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
 public class KnownCardsPanel extends JPanel {
-	//local variables so that we can update the panels when we add cards to the seen
+	//instance variables so that we can update the panels when we add cards to the seen
 	private JPanel playerPanel, roomPanel, weaponPanel;
+	private HumanPlayer humanPlayer;
 	private static Board board;
 	
 	public KnownCardsPanel() {
@@ -35,34 +37,60 @@ public class KnownCardsPanel extends JPanel {
 	}
 	
 	private void updatePanel(JPanel panel, CardType cardType) {
-		HumanPlayer humanPlayer = board.getHumanPlayer();
-		JLabel handLabel = new JLabel("In Hand:");
-		panel.add(handLabel, BorderLayout.SOUTH);
-		
+		humanPlayer = board.getHumanPlayer();
 		Set<Card> inHand = humanPlayer.getHand();
+		ArrayList<Card> correctHandCards = addToCardList(panel, cardType, inHand);
 		
-		for(Card card : inHand) {
-			if(card.getCardType() == cardType) {
-				JTextField textField = new JTextField(15);
-				textField.setText(card.getCardName());
-				panel.add(textField, BorderLayout.SOUTH);
-			}
+		Set<Card> seenCards = humanPlayer.getSeenCards();
+		ArrayList<Card> correctSeenCards = addToCardList(panel, cardType, seenCards);	
+		
+		//2 here so that we can account for the two labels
+		int gridSize = correctHandCards.size() + correctSeenCards.size() + 2;
+		if (correctHandCards.size() == 0) { //so we can add "None" as a Text Field
+			gridSize++;
 		}
 		
-		panel.setLayout(new GridLayout(10,0));
+		if (correctSeenCards.size() == 0) {
+			gridSize++;
+		}
+		
+		panel.setLayout(new GridLayout(gridSize,0));
+		
+		JLabel handLabel = new JLabel("In Hand:");
+		panel.add(handLabel, BorderLayout.SOUTH);
+
+		addTextFields(panel, correctHandCards);
 		
 		JLabel seenLabel = new JLabel("Seen:");
 		panel.add(seenLabel, BorderLayout.SOUTH);
 		
-		Set<Card> seenCards = humanPlayer.getSeenCards();
+		addTextFields(panel, correctSeenCards);
+
 		
-		for(Card card : seenCards) {
-			if(card.getCardType() == cardType) {
+	}
+
+	private void addTextFields(JPanel panel, ArrayList<Card> playerCards) {
+		if (playerCards.size() == 0) { //add only none
+			JTextField textField = new JTextField(15);
+			textField.setText("None");
+			panel.add(textField);
+		} else {
+			for (Card card : playerCards) { //add all the cards
 				JTextField textField = new JTextField(15);
 				textField.setText(card.getCardName());
-				panel.add(textField, BorderLayout.SOUTH);
+				panel.add(textField);
 			}
-		}		
+		}
+	}
+
+	private ArrayList<Card> addToCardList(JPanel panel, CardType cardType, Set<Card> playerCards) {
+		ArrayList<Card> correctCardType = new ArrayList<Card>();
+		for(Card card : playerCards) {
+			if(card.getCardType() == cardType) {
+				correctCardType.add(card);
+			}
+		}
+		return correctCardType; //holds only the player, room, or weapon cards
 	}
 
 	public static void main(String[] args) {
@@ -78,4 +106,5 @@ public class KnownCardsPanel extends JPanel {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
+
 }
